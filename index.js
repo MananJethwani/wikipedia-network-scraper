b3c913139bf94fe8370a582787bdea05a084443a
manan jethwani <mananjethwani02@gmail.com>
2022-04-20 19:05:17 +0530

    updated logic

--- index.js
+++ index.js
@@ -1,45 +1,63 @@
 var axios = require("axios");
 var DomParser = require("dom-parser");
-const createCsvWriter = require("csv-writer").createObjectCsvWriter;
+var createCsvWriter = require("csv-writer").createObjectCsvWriter;
 var parser = new DomParser();
-const csvWriter = createCsvWriter({
-  path: "/home/manan/Desktop/graph2.csv",
+var { sleep, isValidHttpUrl } = require("./utils");
+var csvWriter = createCsvWriter({
+  path: "/home/manan/Desktop/graph.csv",
   header: [
     { id: "node_from", title: "Node From" },
     { id: "node_to", title: "Node To" },
   ],
 });
 
-let restApiUrl = "https://en.wikipedia.org/api/rest_v1/";
-let articlePath = "page/mobile-sections/";
-let apiUrl = "https://en.wikipedia.org/w/api.php?";
-let querString =
+// constant declaration
+const articlePath = "page/mobile-sections/";
+const querString =
   "action=query&format=json&prop=redirects%7Crevisions%7Ccoordinates&rdlimit=max&rdnamespace=0&colimit=max&rawcontinue=true&generator=allpages&gapfilterredir=nonredirects&gaplimit=max&gapnamespace=0&gapcontinue=";
-const map1 = new Map();
+const titleMap = new Map();
+
+// retreiving arguments
+const argv = require("argv");
+const args = argv
+  .option([
+    {
+      name: "URI",
+      short: "u",
+      type: "string",
+      description: "URL for wikipedia to scrape from",
+      example: "'npm run scrape -- --URI=value' or 'npm run scrape -u value'",
+    },
+    {
+      name: "articleList",
+      short: "a",
+      type: "string",
+      description: "Article List to scrape",
+      example:
+        "'npm run scrape -- --articleList=value' or 'npm run scrape -a value'",
+    },
+  ])
+  .run();
+const { URI: baseUri, articleList } = args.options;
+
+// creating base links for fetching
+let restApiUrl = `${baseUri}/api/rest_v1/`;
+let apiUrl = `${baseUri}/w/api.php?`;
 let edges = [];
 let titleList = [];
-
-function sleep(milliseconds) {
-  const date = Date.now();
-  let currentDate = null;
-  do {
-    currentDate = Date.now();
-  } while (currentDate - date < milliseconds);
-}
 
 let appendArticleList = async (url, gapValue) => {
   try {
     sleep(25);
     const { data } = await axios.get(encodeURI(url + gapValue));
-    if (data.query !== undefined && data.query.pages !== undefined) {
+    if (data && data.query && data.query.pages) {
       Object.keys(data.query.pages).forEach(function (key) {
-        if (data.query.pages[key].title !== undefined) {
-          map1.set(data.query.pages[key].title, 0);
-          titleList = [...titleList, data.query.pages[key].title];
+        if (data.query.pages[key].title) {
+          titleList.push(data.query.pages[key].title);
         }
       });
       return (
-        (data["query-continue"] &&
+        (data && data["query-continue"] && data["query-continue"].allpages &&
           data["query-continue"].allpages.gapcontinue) ||
         ""
       );
@@ -56,7 +74,7 @@ contentParser = (html) => {
     const title = element.getAttribute("href");
     if (title === undefined || title === null) return;
     if (title.substring(0, 6) !== "/wiki/") return;
-    titles = [...titles, title.substring(6)];
+    titles.push(title.substring(6));
   });
   return titles;
 };
@@ -67,23 +85,26 @@ async function Dfs(title) {
     const { data } = await axios.get(
       encodeURI(restApiUrl + articlePath + title)
     );
+    if (!data) throw new Error("No data");
     let titles = [];
-    var { sections } = data["lead"];
+    var { sections } = data["lead"] || [];
     for (let index in sections) {
       if (sections[index].text !== undefined) {
-        titles = [...titles, ...contentParser(sections[index].text)];
+        titles = titles.concat(contentParser(sections[index].text));
       }
     }
-    var { sections } = data["remaining"];
+    var { sections } = data["remaining"] || [];
     for (let index in sections) {
       if (sections[index].text !== undefined) {
-        titles = [...titles, ...contentParser(sections[index].text)];
+        titles = titles.concat(contentParser(sections[index].text));
       }
     }
+
     for (let index in titles) {
-      if (map1.get(titles[index]) === 0) {
-        map1.set(titles[index], 1);
-        edges = [...edges, { node_from: title, node_to: titles[index] }];
+      if (titleMap.get(titles[index]) !== undefined && titleMap.get(titles[index]) === 0) {
+        titleMap.set(titles[index], 1);
+        csvWriter.writeRecords([{ node_from: title, node_to: titles[index] }]).then(() => {});
+        edges.push({ node_from: title, node_to: titles[index] });
         await Dfs(titles[index]);
       }
     }
@@ -92,22 +113,48 @@ async function Dfs(title) {
   }
 }
 
-let scrapeArticleList = async () => {
-  var gapValue = "";
-  do {
-    gapValue = await appendArticleList(apiUrl + querString, gapValue);
-  } while (gapValue != "");
-  console.log("here");
+let scrapeArticleList = async (articleList) => {
+  if (articleList === undefined) {
+    var gapValue = "";
+    do {
+      gapValue = await appendArticleList(apiUrl + querString, gapValue);
+    } while (gapValue != "");
+  } else {
+    if (isValidHttpUrl(articleList)) {
+      const { data } = await axios.get(articleList);
+      titleList = data.split("\n");
+    } else {
+      const data = fs.readFileSync(articleList, {
+        encoding: "utf8",
+        flag: "r",
+      });
+      titleList = data.split("\n");
+    }
+  }
+}
+
+let scrapeEdges = async () => {
   for (let index in titleList) {
     console.log(index);
-    if (map1.get(titleList[index]) === 1) continue;
-    map1.set(titleList[index], 1);
+    if (titleMap.get(titleList[index])) continue;
+    titleMap.set(titleList[index], 1);
     await Dfs(titleList[index]);
   }
+}
 
-  csvWriter.writeRecords(edges).then(() => {
-    console.log("...Done");
-  });
+let scrape = async () => {
+  console.log("statring scraping");
+  await scrapeArticleList(articleList);
+  console.log("article list scarpped");
+  titleList.forEach((title) => {
+    titleMap.set(title, 0);
+  })
+  console.log("title list set");
+  await scrapeEdges();
+  console.log("edges scarpped");
+  // csvWriter.writeRecords(edges).then(() => {
+  //   console.log("Saved Edges");
+  // });
 };
 
-scrapeArticleList();
+scrape();
---
+++ utils.js
@@ -0,0 +1,21 @@
+function sleep(milliseconds) {
+  const date = Date.now();
+  let currentDate = null;
+  do {
+    currentDate = Date.now();
+  } while (currentDate - date < milliseconds);
+}
+
+function isValidHttpUrl(string) {
+  let url;
+
+  try {
+    url = new URL(string);
+  } catch (_) {
+    return false;
+  }
+
+  return url.protocol === "http:" || url.protocol === "https:";
+}
+
+module.exports = {sleep, isValidHttpUrl};
